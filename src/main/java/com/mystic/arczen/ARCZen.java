df35127294a2d05b4f67fd54ac56fb8670748c11
Mysticpasta1 <ethana5tc@outlook.com>
2022-11-26 13:08:56 -0600

    got the bot up and running
    added hello command, greet command, and ping command

---
+++ src/main/java/com/mystic/arczen/ARCZen.java
@@ -0,0 +1,36 @@
+import com.novamaday.d4j.gradle.simplebot.listeners.SlashCommandListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class SimpleBot {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleBot.class);
+
+    public static void main(String[] args) {
+        //Creates the gateway client and connects to the gateway
+        final GatewayDiscordClient client = DiscordClientBuilder.create(System.getenv("BOT_TOKEN")).build()
+            .login()
+            .block();
+
+        /* Call our code to handle creating/deleting/editing our global slash commands.
+        We have to hard code our list of command files since iterating over a list of files in a resource directory
+         is overly complicated for such a simple demo and requires handling for both IDE and .jar packaging.
+         Using SpringBoot we can avoid all of this and use their resource pattern matcher to do this for us.
+         */
+        List<String> commands = List.of("greet.json", "ping.json");
+        try {
+            new GlobalCommandRegistrar(client.getRestClient()).registerCommands(commands);
+        } catch (Exception e) {
+            LOGGER.error("Error trying to register global slash commands", e);
+        }
+
+        //Register our slash command listener
+        client.on(ChatInputInteractionEvent.class, SlashCommandListener::handle)
+            .then(client.onDisconnect())
+            .block(); // We use .block() as there is not another non-daemon thread and the jvm would close otherwise.
+    }
+}
